c919777afcd789da4023e9ba608e1a3626dca17d
Kristiyan Kanchev <kristiyankan4ev@abv.bg>
2021-06-09 20:24:47 +0300

    Update example.py

--- cogs/example.py
+++ cogs/example.py
@@ -9,6 +9,7 @@ class Example(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        number = 0
 
     @commands.command(aliases=('num?',))
     async def test(self, ctx, num: int):
@@ -16,6 +17,11 @@ class Example(commands.Cog):
         The main command which handles the code execution process.
         """
         await ctx.send(f"The number entered {num}")
+       
+    @commands.command(aliases=('num'))
+    async def test(self, ctx):
+        await ctx.send(f"The number entered {number}")
+        number += 1
 
 def setup(bot):
     bot.add_cog(Example(bot))
